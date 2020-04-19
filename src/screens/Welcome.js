0d3295bf07b78d2d8e800748957979fb9e697cbf
Alan <lanzn16@outlook.com>
2020-04-18 22:54:22 -0300

    add welcome screen

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
+import ButtonOpacity from './components/ButtonOpacity';
+
 const styles = StyleSheet.create({
   root: {
     flex: 1,
+  },
+  content: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+
+    margin: 10,
+  },
+  started: {
+    flex: 0.2,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    margin: 10,
+  },
+  contentImage: {
+    flex: 1,
+    justifyContent: 'center',
+
+    margin: 10,
+  },
+  image: {
+    width: 30,
+  },
+  contentText: {
+    flex: 0.2,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginHorizontal: 50,
+
+    margin: 10,
+  },
+  titleContent: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  textContent: {
+    fontSize: 17,
+    color: '#d0d0d0',
+    textAlign: 'center',
   },
 });
 
@@ -13,7 +53,21 @@ class Welcome extends React.PureComponent {
   render() {
     return (
       <View style={styles.root}>
-        <Text> Welcome </Text>
+        <View style={styles.content}>
+          <View style={styles.contentImage}>
+            <Text> Images. </Text>
+          </View>
+          <View style={styles.contentText}>
+            <Text style={styles.titleContent}> Secured, forever. </Text>
+            <Text style={styles.textContent}>
+              Nam sit amet nisl ut ipsum ullamcorper vulputate et at enim.
+              Vestibulum nulla est, tincidunt et est pharetra consectetur.
+            </Text>
+          </View>
+        </View>
+        <View style={styles.started}>
+          <ButtonOpacity title="GET STARTED" onPress={() => {}} />
+        </View>
       </View>
     );
   }
